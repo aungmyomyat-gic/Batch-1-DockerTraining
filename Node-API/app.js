099fd454340ffb3e44d45cbdacd5085b51d05d63
Aung Myo Myat <aungmyomyat@gicjp.com>
2025-01-17 11:53:20 +0630

    Add Docker file

--- Node-API/app.js
+++ Node-API/app.js
@@ -5,19 +5,19 @@ const app = express()
 
 app.use(cors())
 
-app.get('/', (req, res) => {
+app.get('/books', (req, res) => {
   res.json([
     {
       "id":"1",
-      "title":"Book Review: The Bear & The Nightingale"
+      "Pone Nya Khin":"Lat Kyan La Yaung"
     },
     {
       "id":"2",
-      "title":"Game Review: Pokemon Brillian Diamond"
+      "Khat Zaw":"The guys from yangon..."
     },
     {
       "id":"3",
-      "title":"Show Review: Alice in Borderland"
+      "Khat Zaw":"The guys from mandalay"
     }
   ])
 })
