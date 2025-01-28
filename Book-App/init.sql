9af8299a029e50dbc455c765e72d91332369b38f
Aung Myo Myat <aungmyomyat@gicjp.com>
2025-01-28 10:35:56 +0630

    Fix Error

--- Book-App/init.sql
+++ Book-App/init.sql
@@ -4,3 +4,9 @@ CREATE TABLE IF NOT EXISTS books (
     author VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 ); 
+
+INSERT INTO books (title, author) VALUES 
+    ('The Great Gatsby', 'F. Scott Fitzgerald'),
+    ('To Kill a Mockingbird', 'Harper Lee'),
+    ('1984', 'George Orwell'),
+    ('Pride and Prejudice', 'Jane Austen'); 
